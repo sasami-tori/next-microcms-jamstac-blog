10d208ec28b6abcacbb9cd83af50134a22ce35ee
sasamitori <askmonaka@gmail.com>
2022-09-25 11:36:08 +0900

    second commit

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import { client } from '../libs/client';
+import Link from "next/link";
+import styles from '../styles/Home.module.scss'
+
+//SSG
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    <div className={styles.container}>
+      {blog.map((blog) => (
+        <li key={blog.id}>
+          <Link href={`blog/${blog.id}`}>
+            <a href="">{blog.title}</a>
+          </Link>
+        </li>
+      ))}
+    </div >
+  )
+}
